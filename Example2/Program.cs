4824d377357232fbe750066fa6652f0cd35e2c34
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-21 23:56:02 +0300

    new folder Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+Console.Clear();
+
+/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно */
+
+Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+int result = x/y;
+bool result2 = x%y==0;
+if (x%y==0)
+{
+    Console.WriteLine($"{x}, {y}-> кратно");
+}
+else
+{
+    Console.WriteLine($"{x}, {y} -> не кратно, остаток {x/y}");
+}
