0946371fe42b49a623751f79071011d6af4707ab
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-23 00:14:22 +0300

    new folde Task10r

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+Console.Clear();
+
+/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите трехзначное число");
+int N=Convert.ToInt32(Console.ReadLine());
+N = N[1];
+Console.WriteLine(result);
