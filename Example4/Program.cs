c6e7151d121696a30a1058863d4b59efd59bdbb3
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-22 16:52:39 +0300

    new folder Example4

---
+++ Example4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.Clear();
+
+/*Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого. */
+
+Console.WriteLine("Введите первое число");
+int first=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int second=Convert.ToInt32(Console.ReadLine());
+
+int work=second*second;
+
+
+string sum=(first>second&&first==work)?"Является":"Не является";
+Console.WriteLine(sum);
