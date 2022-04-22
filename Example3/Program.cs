3e4cd83174ef82fbf19f2cedaf9504d205ad8a60
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-22 16:12:04 +0300

    new folder Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+Console.Clear();
+
+/*Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+14 -> нет
+46 -> нет
+161 -> да */
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+string answer=(num%7==00 && num%23==00)?$"{num} -> да":$"{num} -> нет";
+Console.WriteLine(answer);
