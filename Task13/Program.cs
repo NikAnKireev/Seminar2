9da20afe4db92f4192e20b1deacecea0934371bd
Nikita Kireev <kireevinvest@yandex.ru>
2022-04-23 19:21:51 +0300

    new folder task13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Console.Clear();
+
+/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+Console.WriteLine("Введите число");
+int N=Convert.ToInt32(Console.ReadLine());
+string numS=N.ToString();
+int length = numS.Length;
+string result=(length>=3)?$"{N} -> {numS[2]}":$"{N} -> третьей цифры нет ";
+Console.WriteLine(result);
